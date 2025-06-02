84e47ce231397f312ec03dc259e1c6a8967daef9
Thiago.comba <thiagocomba@hotmail.com>
2025-06-02 10:54:21 -0300

    Templates

--- mysite/blog/factories.py
+++ mysite/blog/factories.py
@@ -8,12 +8,14 @@ from blog.models import Post
 
 faker = FakerFactory.create()
 
+
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
 
     email = factory.Faker("safe_email")
     username = factory.LazyAttribute(lambda x: faker.name())
+
 
     @classmethod
     def _prepare(cls, create, **kwargs):
@@ -24,13 +26,13 @@ class UserFactory(factory.django.DjangoModelFactory):
             if create:
                 user.save()
         return user
-
+        
 class PostFactory(factory.django.DjangoModelFactory):
     title = factory.LazyAttribute(lambda x: faker.sentence())
     created_on = factory.LazyAttribute(lambda x: now())
     author = factory.SubFactory(UserFactory)
     status = 0
 
+
     class Meta:
         model = Post
-
